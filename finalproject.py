78778971639a222df41e010ee486fadc95fc2454
Alana Woloshin <awoloshi@umich.edu>
2019-04-08 16:19:30 -0400

    added api key for yelp

--- finalproject.py
+++ finalproject.py
@@ -4,7 +4,9 @@
 import requests
 import sqlite3
 
-
+class yelpinfo():
+    def __init__(self,api.key):
+        self.api.key = 'pquYov7zgyZLjMvl3Mhy0a5vW-8KExfOhaJ1VptNUsU94anF46fnyByx4a9h4qAtE2jfyWQPunZsIvuD9dL9Nf-uVU1zaIud-bYTFDu0le6NHo62I54N4wxVB6yrXHYx'
 def get_data(database):
     conn = sqlite3.connect(database)
     cur = conn.cursor()
@@ -13,4 +15,3 @@ def get_data(database):
     data = cur.fetchall()
     params_dict = {'Authorization': 'Bearer' + yelpinfo.api.key}
     r1 = requests.get('https://api.yelp.com/v3/businesses/search?location=Amsterdam&categories=dinner&limit=25', params = params_dict)
-    r2 = requests.get('https://api.yelp.com/v3/businesses/search?location=Italy&categories=dinner&limit=25', params = params_dict)
