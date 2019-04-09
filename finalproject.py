49509ef20246523894080c8226958c956592be1e
Alana Woloshin <awoloshi@umich.edu>
2019-04-09 09:18:37 -0400

    data base for yelp

--- finalproject.py
+++ finalproject.py
@@ -15,8 +15,16 @@ def get_data(database):
     data = cur.fetchall()
 
     params_dict = {'Authorization': 'Bearer' + yelpinfo.api}
-    r1 = requests.get('https://api.yelp.com/v3/businesses/search?location=Amsterdam&categories=dinner&limit=25', params = params_dict)
-    r2 = requests.get("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/v1.0",headers={
+    r1 = requests.get('https://api.yelp.com/v3/businesses/search?location=Amsterdam&categories=dinner&limit=20', params = params_dict)
+    for x in r1.json()['businesses']:
+      rating = x['rating']
+      restaurant_name= x['name']
+      reviews= x['review_count']
+      city= x['location']['city']
+      cur.execute('INSERT INTO Restaurants (restaurant_name, rating, reviews, city) VALUES (?, ?, ?, ?)', (restaurant_name, rating, reviews, city))
+    conn.commit()
+
+    r2 = requests.post("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/v1.0",headers={
     "X-RapidAPI-Host": skyscanner.apihost, "X-RapidAPI-Key": skyscanner.apikey, "Content-Type": "application/x-www-form-urlencoded"}, params ={
     "inboundDate": "2019-06-10",
     "cabinClass": "business",
