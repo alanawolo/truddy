09aa8d49774a3622f117a31b51136139ccfd9eed
Alana Woloshin <awoloshi@umich.edu>
2019-04-10 14:40:50 -0400

    fixed weatehr ish

--- finalproject.py
+++ finalproject.py
@@ -24,19 +24,22 @@ def get_data(database):
       cur.execute('INSERT INTO Restaurants (name, rating, reviews, country) VALUES (?, ?, ?, ?)', (restaurant_name, rating, reviews, city))
     conn.commit()
 
-    cur.execute('CREATE TABLE IF NOT EXISTS Weather (main TEXT, description TEXT, temp INTEGER, humidity INTEGER)')
+    cur.execute('CREATE TABLE IF NOT EXISTS Weather ( temp INTEGER, pres, INTEGER, humidity INTEGER, mintemp INTEGER, maxtemp INTEGER)')
     cur.execute('SELECT * FROM Weather')
     data = cur.fetchall()
     
     #this ket should be updated in a couple hours
     r2 = requests.get('https://api.openweathermap.org/data/2.5/weather/?q=Amsterdam&cnt=20&APPID=318f31fd15810fe42b21f896c93c2779')
     print(r2.json())
-    #for x in r2.json()['weather']:
-      #main = x['weather']['main']
-      #desc= x['weather']['description']
-      #temp= x['temp']
-      #hum= x['humidity']
-      #cur.execute('INSERT INTO Weather (main, description, temp, humidity) VALUES (?, ?, ?, ?)', (main, desc, temp, hum))
+    templst = []
+
+    for x in r2.json()['main'].items():
+      print(x)
+      #temp= x[1]
+      #hum= x[2]
+      #mintemp = x[3]
+      #maxtemp = x[4]
+      #cur.execute('INSERT INTO Weather (temp, humidity, pressure, mintemp,maxtemp) VALUES (?, ?, ?, ?)', (temp,pres, hum, mintemp,maxtemp))
     #conn.commit()
 
     cur.execute('CREATE TABLE IF NOT EXISTS Playlist (artistName TEXT, trackName TEXT, contentRatingAdvisory TEXT, trackTimeMillis)')
@@ -45,7 +48,6 @@ def get_data(database):
 
     params_dict = params = {'term': 'Amsterdam', 'country': "US", 'media': 'music'}
     r3 = requests.get('https://itunes.apple.com/search?', params = params_dict)
-    print(r3.json())
     for x in r3.json()['businesses']:
       artistName = x['artistName']
       trackName= x['trackName']
