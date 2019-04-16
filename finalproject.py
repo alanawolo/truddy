3b128b8a9121e463229dee85e852e3b1e07dcbcc
Rachel Becker <rachelbecker@Rachels-MacBook-Pro-6.local>
2019-04-16 15:22:37 -0400

    more stuff

--- finalproject.py
+++ finalproject.py
@@ -25,23 +25,20 @@ def get_data(database):
   conn.commit()
 
   # Event Data
-  cur.execute('CREATE TABLE IF NOT EXISTS Events (name TEXT, priceMin Integer, priceMax Integer)')
+  cur.execute('CREATE TABLE IF NOT EXISTS Events (name TEXT, priceMin Integer UNIQUE, priceMax Integer)')
   cur.execute('SELECT * FROM Events')
   data = cur.fetchall()
   r2 = requests.get("https://app.ticketmaster.com/discovery/v2/events?apikey=J0BKwKyQOuE9li4P2HDD1J4Ho2JWug95&size=20&countryCode=NL")
   for x in r2.json()['_embedded']['events']:
     print(x)
     name = x['name']
-    print(name)
     priceMin = x['priceRanges'][1]['min']
-    print(priceMin)
     priceMax = x['priceRanges'][1]['max']
-    print(priceMax)
     cur.execute('INSERT OR IGNORE INTO Events (name, priceMin, priceMax) VALUES (?, ?, ?)', (name, priceMin, priceMax))
   conn.commit()
 
   # Playlist Data
-  cur.execute('CREATE TABLE IF NOT EXISTS Playlist (artistName TEXT, trackName TEXT, trackTimeMillis INT)')
+  cur.execute('CREATE TABLE IF NOT EXISTS Playlist (artistName TEXT, trackName TEXT, trackTimeMillis INT UNIQUE)')
   cur.execute('SELECT * FROM Playlist')
   data = cur.fetchall()
   params_dict = {'term': 'travel', 'country': 'US', 'media': 'music'}
@@ -70,6 +67,26 @@ def get_data(database):
   average_restaurant_rating = (total/count)
   print(average_restaurant_rating)
 
+# Events
+# Average maximum price
+  cur.execute('SELECT priceMax FROM Events')
+  total = 0
+  count = 0
+  for price in cur:
+    total += price[0]
+    count += 1
+  average_max_price = (total/count)
+  print(average_max_price)
+
+# Average minimum price
+  cur.execute('SELECT priceMin FROM Events')
+  total = 0
+  count = 0
+  for price2 in cur:
+    total += price2[0]
+    count += 1
+  average_min_price = (total/count)
+  print(average_min_price)
 
 # Playlist
 # average song length
@@ -105,7 +122,7 @@ def get_data(database):
       data[x] = ratings_list[count]
     count += 1 
 
-  plt.bar(name_list[0:5], ratings_list[0:5], align='center', color = ['magenta', 'red', 'indigo', 'blue', 'orange', 'pink', 'purple', 'violet', 'green', 'black', 'gray', 'yellow', 'navy', 'teal', 'aquamarine', 'cyan', 'lime', 'blueviolet', 'lavender', 'plum'])
+  plt.bar(name_list[0:5], ratings_list[0:5], align='center', color = ['magenta', 'indigo', 'blue', 'teal', 'aquamarine'])
   plt.ylabel('Rating')
   plt.xlabel('Restaurant Name')
   plt.title('Ratings of Amsterdam Restaurants')
@@ -119,7 +136,7 @@ def get_data(database):
     song_list.append(song[0])
 
   cur.execute('SELECT trackTimeMillis FROM Playlist')
-  times_list = []
+  times_list = []git
   for time in cur:
     time_in_seconds = time[0] * (0.001/1)
     times_list.append(time_in_seconds)
@@ -131,12 +148,41 @@ def get_data(database):
       data[x] = times_list[count2]
     count2 += 1 
 
+  plt.bar(song_list[0:5], times_list[0:5], align='center', color = ['magenta', 'indigo', 'blue', 'teal', 'aquamarine'])
+  plt.ylabel('Time')
+  plt.xlabel('Song Name')
+  plt.title('Length of Songs')
+  plt.savefig('Playlist_Plot.png')
+  plt.show() 
+
+# Events plot ... plotting the max price for top 5 events
+  cur.execute('SELECT name FROM Events')
+  name_list = []
+  for name in cur:
+    name_list.append(name[0])
+
+  cur.execute('SELECT priceMin FROM Events')
+  min_list = []
+  for min_price in cur:
+    min_list.append(min_price[0])
+
+  data = {}
+  count3 = 0
+  for x in song_list:
+    if x not in data:
+      data[x] = times_list[count2]
+    count3 += 1 
+
   plt.bar(song_list[0:5], times_list[0:5], align='center', color = ['magenta', 'red', 'indigo', 'blue', 'orange', 'pink', 'purple', 'violet', 'green', 'black', 'gray', 'yellow', 'navy', 'teal', 'aquamarine', 'cyan', 'lime', 'blueviolet', 'lavender', 'plum'])
   plt.ylabel('Time')
   plt.xlabel('Song Name')
   plt.title('Length of Songs')
   plt.savefig('Playlist_Plot.png')
   plt.show() 
+
+
+
+
 
   return 'Database Created'
 print(get_data('final_project.sqlite'))
